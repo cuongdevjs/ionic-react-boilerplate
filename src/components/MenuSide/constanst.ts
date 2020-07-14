2b5ec92831b148285482603f0af235d1b0b66779
Cuongdevjs <nguyenmanhcuong.stf@gmail.com>
2020-07-14 20:40:24 +0700

    update: url menu sidebar

--- src/components/MenuSide/constanst.ts
+++ src/components/MenuSide/constanst.ts
@@ -1,47 +1,47 @@
 import {
-  bookmarkOutline,
-  createOutline,
-  newspaperOutline,
-  callOutline,
-  settingsOutline,
-  medalOutline,
-  homeOutline,
+	bookmarkOutline,
+	createOutline,
+	newspaperOutline,
+	callOutline,
+	settingsOutline,
+	medalOutline,
+	homeOutline,
 } from "ionicons/icons";
 
 export const LIST_MENU_ITEM = [
-  {
-    icon: homeOutline,
-    href: "/home",
-    label: "Trang chủ",
-  },
-  {
-    icon: bookmarkOutline,
-    href: "/campaign",
-    label: "Chiến dịch",
-  },
-  {
-    icon: createOutline,
-    href: "/create-campaign",
-    label: "Tạo chiến dịch",
-  },
-  {
-    icon: newspaperOutline,
-    href: "/news",
-    label: "Tin tức",
-  },
-  {
-    icon: settingsOutline,
-    href: "/support",
-    label: "Hỗ trợ",
-  },
-  {
-    icon: callOutline,
-    href: "/contact",
-    label: "Liên hệ",
-  },
-  {
-    icon: medalOutline,
-    href: "/about",
-    label: "Về Yoolove",
-  },
+	{
+		icon: homeOutline,
+		href: "/home",
+		label: "Trang chủ",
+	},
+	{
+		icon: bookmarkOutline,
+		href: "/campaign",
+		label: "Chiến dịch",
+	},
+	{
+		icon: createOutline,
+		href: "/campaign/create",
+		label: "Tạo chiến dịch",
+	},
+	{
+		icon: newspaperOutline,
+		href: "/news",
+		label: "Tin tức",
+	},
+	{
+		icon: settingsOutline,
+		href: "/support",
+		label: "Hỗ trợ",
+	},
+	{
+		icon: callOutline,
+		href: "/contact",
+		label: "Liên hệ",
+	},
+	{
+		icon: medalOutline,
+		href: "/about",
+		label: "Về Yoolove",
+	},
 ];
