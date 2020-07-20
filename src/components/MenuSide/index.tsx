cf7e8e2ddb43c57235d2753ca66f573e185456af
Cuongdevjs <nguyenmanhcuong.stf@gmail.com>
2020-07-20 09:21:24 +0700

    fix: blank page initialize app

--- src/components/MenuSide/index.tsx
+++ src/components/MenuSide/index.tsx
@@ -3,60 +3,62 @@
  * MenuSide
  *
  */
-import React, { memo } from "react";
+import React, { memo } from 'react'
 
-import { IonMenu, IonList } from "@ionic/react";
-import { menuController } from "@ionic/core";
-import { MenuSideWrapper, MenuContent } from "./styled";
-import { I_InfoMySelf } from "pages/App/types";
-import { Header, Footer, MenuItem } from "./components";
-import { LIST_MENU_ITEM } from "./constanst";
-import { useHistory } from "react-router";
+import { IonMenu, IonList } from '@ionic/react'
+import { menuController } from '@ionic/core'
+import { MenuSideWrapper, MenuContent } from './styled'
+import { I_InfoMySelf } from 'pages/App/types'
+import { Header, Footer, MenuItem } from './components'
+import { LIST_MENU_ITEM } from './constanst'
+import { useHistory } from 'react-router'
 
 interface Props {
-  isLogged: boolean;
-  infoMySelf: I_InfoMySelf;
-  logout: () => void;
+  isLogged: boolean
+  infoMySelf: I_InfoMySelf
+  logout: () => void
 }
 
 export const MenuSide = memo((props: Props) => {
-  const history = useHistory();
-  React.useEffect(() => {
-    menuController.enable(true);
-  }, []);
+  const history = useHistory()
 
   const _onNavigation = React.useCallback(
     (url: string) => {
-      menuController.close();
-      history.push(url);
+      menuController.close()
+      history.push(url)
     },
     [history]
-  );
+  )
 
   const _onLogout = React.useCallback(() => {
-    const logout = props.logout;
-    logout();
-    _onNavigation("/login");
-  }, [_onNavigation, props.logout]);
+    const logout = props.logout
+    logout()
+    _onNavigation('/login')
+  }, [_onNavigation, props.logout])
+
+  React.useEffect(() => {
+    menuController.enable(true)
+    _onNavigation('/')
+  }, [_onNavigation])
 
   return (
     <MenuSideWrapper>
       <IonMenu
-        side="start"
-        contentId="menuMain"
-        className="my-custom-menu"
+        side='start'
+        contentId='menuMain'
+        className='my-custom-menu'
         // type="overlay"
       >
         <Header
           isLogged={props.isLogged}
           infoMySelf={props.infoMySelf}
-          onToProfilePage={() => _onNavigation("/profile")}
-          onToLoginPage={() => _onNavigation("/login")}
+          onToProfilePage={() => _onNavigation('/profile')}
+          onToLoginPage={() => _onNavigation('/login')}
         />
         <MenuContent>
           {/* <IonContent id="content"> */}
           <IonList>
-            {LIST_MENU_ITEM.map((item) => (
+            {LIST_MENU_ITEM.map(item => (
               <MenuItem
                 key={item.label + item.href}
                 label={item.label}
@@ -70,5 +72,5 @@ export const MenuSide = memo((props: Props) => {
         <Footer isLogged={props.isLogged} logout={_onLogout} />
       </IonMenu>
     </MenuSideWrapper>
-  );
-});
+  )
+})
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { IonApp, IonRouterOutlet, IonToast } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
+import React from 'react'
+import { IonApp, IonRouterOutlet, IonToast } from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
 /* Theme variables */
-import "../../theme/variables.css";
-import "antd-mobile/dist/antd-mobile.css";
-import { GlobalStyle } from "../../global/styted";
+import '../../theme/variables.css'
+// import 'antd-mobile/dist/antd-mobile.css'
+import { GlobalStyle } from '../../global/styted'
 
-import { Route } from "react-router";
-import { useSelector, useDispatch } from "react-redux";
+import { Route } from 'react-router'
+import { useSelector, useDispatch } from 'react-redux'
 import {
   selectIsLogged,
   selectIsShowToast,
@@ -33,55 +33,55 @@ import {
   selectHeaderToast,
   selectTranslucentToast,
   selectPositionToast,
-  selectInfoMySelf,
-} from "./selectors";
-import { useInjectReducer, useInjectSaga } from "utils/redux-injectors";
-import { sliceKey, reducer, actions, GET_INFO_MYSELF } from "./slice";
-import { appSaga } from "./saga";
-import { getItem } from "utils/lsStorage";
+  selectInfoMySelf
+} from './selectors'
+import { useInjectReducer, useInjectSaga } from 'utils/redux-injectors'
+import { sliceKey, reducer, actions, GET_INFO_MYSELF } from './slice'
+import { appSaga } from './saga'
+import { getItem } from 'utils/lsStorage'
 //
-import MenuSide from "components/MenuSide/Loadable";
-import { Routers } from "./Routers";
+import MenuSide from 'components/MenuSide/Loadable'
+import { Routers } from './Routers'
 
 interface Props {}
 
 export const App = () => {
-  useInjectReducer({ key: sliceKey, reducer: reducer });
-  useInjectSaga({ key: sliceKey, saga: appSaga });
-  const dispatch = useDispatch();
-  const isLogged = useSelector(selectIsLogged);
-  const isShowToast = useSelector(selectIsShowToast);
-  const messageToast = useSelector(selectMessageToast);
-  const durationToast = useSelector(selectDurationToast);
-  const colorToast = useSelector(selectColorToast);
-  const headerToast = useSelector(selectHeaderToast);
-  const translucentToast = useSelector(selectTranslucentToast);
-  const positionToast = useSelector(selectPositionToast);
-  const infoMySelf = useSelector(selectInfoMySelf);
+  useInjectReducer({ key: sliceKey, reducer: reducer })
+  useInjectSaga({ key: sliceKey, saga: appSaga })
+  const dispatch = useDispatch()
+  const isLogged = useSelector(selectIsLogged)
+  const isShowToast = useSelector(selectIsShowToast)
+  const messageToast = useSelector(selectMessageToast)
+  const durationToast = useSelector(selectDurationToast)
+  const colorToast = useSelector(selectColorToast)
+  const headerToast = useSelector(selectHeaderToast)
+  const translucentToast = useSelector(selectTranslucentToast)
+  const positionToast = useSelector(selectPositionToast)
+  const infoMySelf = useSelector(selectInfoMySelf)
   /* *
-  * get token is promise async => process done => useRouter
-  * avoid the case redirect to /login when isLogged still doesn't check done yet,
-  * and after check done => /login redirect to /home => wrong process
-  * */
-  const [isCheckLoggedDone, setIsCheckLoggedDone] = React.useState(false);
+   * get token is promise async => process done => useRouter
+   * avoid the case redirect to /login when isLogged still doesn't check done yet,
+   * and after check done => /login redirect to /home => wrong process
+   * */
+  const [isCheckLoggedDone, setIsCheckLoggedDone] = React.useState(false)
 
   const checkLogged = React.useCallback(async () => {
-    const token = await getItem("yoolove-token");
-    dispatch(actions.changeIsLogged({ stateLogged: !!token }));
-    setIsCheckLoggedDone(true);
-  }, [dispatch]);
+    const token = await getItem('yoolove-token')
+    dispatch(actions.changeIsLogged({ stateLogged: !!token }))
+    setIsCheckLoggedDone(true)
+  }, [dispatch])
 
   React.useEffect(() => {
-    checkLogged();
-  }, [checkLogged]);
+    checkLogged()
+  }, [checkLogged])
 
   React.useEffect(() => {
-    if (isLogged) dispatch(GET_INFO_MYSELF());
-  }, [dispatch, isLogged]);
+    if (isLogged) dispatch(GET_INFO_MYSELF())
+  }, [dispatch, isLogged])
 
   const _onLogout = React.useCallback(() => {
-    dispatch(actions.logout());
-  }, [dispatch]);
+    dispatch(actions.logout())
+  }, [dispatch])
 
   return (
     <IonApp>
@@ -91,14 +91,16 @@ export const App = () => {
           infoMySelf={infoMySelf}
           logout={_onLogout}
         />
-        <IonRouterOutlet animated={false} id="menuMain">
-          <Route
-            path="/"
-            render={(props) => {
-              return isCheckLoggedDone && <Routers {...props} />;
-            }}
-          />
-        </IonRouterOutlet>
+        {isCheckLoggedDone && (
+          <IonRouterOutlet animated={false} id='menuMain'>
+            <Route
+              path='/'
+              render={props => {
+                return <Routers {...props} />
+              }}
+            />
+          </IonRouterOutlet>
+        )}
       </IonReactRouter>
       <GlobalStyle />
       <IonToast
@@ -114,14 +116,14 @@ export const App = () => {
           durationToast === 0
             ? [
                 {
-                  text: "OK",
-                  role: "cancel",
-                  handler: () => {},
-                },
+                  text: 'OK',
+                  role: 'cancel',
+                  handler: () => {}
+                }
               ]
             : []
         }
       />
     </IonApp>
-  );
-};
+  )
+}
